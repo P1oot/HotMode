11b056013258fb9a584f6bac9369dabd5200a8fa
Dan <dan.berdinskikh@mail.ru>
2023-12-20 16:08:43 +0300

    Add barbers to admin

--- hot_mode/haircuts/admin.py
+++ hot_mode/haircuts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Services, Haircuts, Promotions
+from .models import Services, Haircuts, Promotions, Barbers
 
 
 @admin.register(Services)
@@ -10,6 +10,11 @@ class ServicesAdmin(admin.ModelAdmin):
 
 @admin.register(Promotions)
 class PromotionsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Barbers)
+class BarbersAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 
